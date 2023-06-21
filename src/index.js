b91deb0afa1f6390514f88194e54e8be191e6525
WinterJackson <winterjacksonwj@gmail.com>
2023-06-21 20:35:31 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const todoInput = document.querySelector('#new-task-description');
+    buildTodoItem(todoInput.value);
+  });
 });
+
+function buildTodoItem(todo) {
+  const p = document.createElement('p');
+  const btn = document.createElement('button'); // corrected the 'button' tag name
+  btn.textContent = 'x';
+  btn.addEventListener('click', handleDelete);
+  p.textContent = todo;
+  p.appendChild(btn); // appended the button to the paragraph
+  document.querySelector('#list').appendChild(p);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
